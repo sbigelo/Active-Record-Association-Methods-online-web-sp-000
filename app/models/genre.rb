5bc134bd268a95c3eb258c0392b9355af7f65e85
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-01 22:33:43 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,6 +15,6 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
 
-    self.artists.name.each {|key| key}
+    self.artists.name
   end
 end
